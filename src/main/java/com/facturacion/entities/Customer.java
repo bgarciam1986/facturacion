e583ae118dfd1287db7f6cf5f665f61193d1cea3
bgarciam <bgarciam@live.com.mx>
2021-06-06 23:12:04 -0500

    Editamos la class product

---
+++ src/main/java/com/facturacion/entities/Customer.java
@@ -0,0 +1,126 @@
+package com.facturacion.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "paternal_surname")
+	private String paternalSurname;
+	
+	@Column(name = "maternal_surname")
+	private String maternalSurname;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "creat_at")
+	private Date createAt;
+	
+	// realacion entre clientes y facturas n->m
+	@OneToMany(mappedBy = "customer",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	public List<Invoice> invoices;
+	
+	public Customer() {
+		// Constructor vacio, incializa el arreglo de invoices
+		invoices = new ArrayList<Invoice>();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPaternalSurname() {
+		return paternalSurname;
+	}
+	public void setPaternalSurname(String paternalSurname) {
+		this.paternalSurname = paternalSurname;
+	}
+	
+	public String getMaternalSurname() {
+		return maternalSurname;
+	}
+	public void setMaternalSurname(String maternalSurname) {
+		this.maternalSurname = maternalSurname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	public List<Invoice> getInvoices() {
+		return invoices;
+	}
+
+	public void setInvoices(List<Invoice> invoices) {
+		this.invoices = invoices;
+	}
+	
+	// Crea nueva fecha en variable
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+	
+	// Agrega factura al cliente invoice*invoice
+	public void addInvoice(Invoice invoice) {
+		invoices.add(invoice);
+	}
+
+	@Override
+	public String toString() {
+		return "Customer[id="+id+", firstName="+firstName+", lastName="+lastName+",paternalSurname="+paternalSurname+",maternalSurname="+maternalSurname+",email="+email+",createAt="+createAt+", invoices="+invoices+"]";
+	}
+
+}
---
+++ src/main/java/com/facturacion/entities/Invoice.java
@@ -0,0 +1,113 @@
+package com.facturacion.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "invoices")
+public class Invoice implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "observation")
+	private String observation;
+	
+	@Column(name = "create_at")
+	private Date createAt;
+	
+	// Relacion entre invoice y customer m->n
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Customer customer;
+	
+	// Relacion en un solo sentido, por eso debemos indicar la columna que se creara automaticamente
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "invoice_id")
+	private List<ItemInvoice> items;
+	
+	public Invoice() {
+		// Inicializamos el arreglo de los invoices
+		this.items = new ArrayList<ItemInvoice>();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getObstevation() {
+		return observation;
+	}
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+	
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	public List<ItemInvoice> getItems(){
+		return items;
+	}
+	public void setItems(List<ItemInvoice> items) {
+		this.items = items;
+	}
+	
+	public void addItemInvoice(ItemInvoice item) {
+		items.add(item);
+	}
+	
+	// inicializamos una nueva fecha en var createAt
+	@PrePersist
+	public void prePersist() {
+		createAt  = new Date();
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer[id="+id+", description="+description+", observation="+observation+", createAt="+createAt+", customer="+customer+"]";
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
